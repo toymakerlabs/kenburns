7020918ed5e5d8045adee5d84e64c8d45a365ceb
John <toymakerlabs@gmail.com>
2012-11-18 16:24:27 -0800

    Wrapped images in divs to separate animation effects

--- js/kenburns.js
+++ js/kenburns.js
@@ -32,6 +32,7 @@ john@toymakerlabs.com
         defaults = {
             images:[],
             duration:400,
+            fadeSpeed:500,
             scale:1,
             onLoadingComplete:function(){},
             onSlideComplete:function(){},
@@ -68,6 +69,9 @@ john@toymakerlabs.com
        // var container = $(this.element);
         var list = this.options.images;
         var that = this;
+
+        this.width = $(this.element).width();
+        this.height = $(this.element).height();
         // for (i in list) {
         // 	this.attachImage(list[i], "image"+i , i);
         // 	imagesObj["image"+i] = {};
@@ -98,14 +102,23 @@ john@toymakerlabs.com
     //Load Images in parallell but keep track of the order. 
     Plugin.prototype.attachImage = function(url,alt_text,index) {
     	var that = this;
+
+        //put the image in an empty div to separate the animation effects of fading and moving
+        var wrapper = $('<div/>');
+        wrapper.attr('class','kb-slide');
+
 		var img = $("<img />");
 		img.attr('src', url);
 		img.attr('alt', alt_text);
 
+        wrapper.html(img);
+
         img.load(function() {
         	imagesObj["image"+index].element = this;
         	imagesObj["image"+index].loaded  = true;
-            that.insertAt(index,this);
+            imagesObj["image"+index].width = this.width;
+            imagesObj["image"+index].height = this.height;
+            that.insertAt(index,wrapper);
             that.resume(index);
 		});
 	}
@@ -122,7 +135,7 @@ john@toymakerlabs.com
         // it will then resume the transition.
         if(index == this.holdup) {
             console.log("resuming");
-           // this.startTransition(this.holdup);
+            this.startTransition(this.holdup);
         }
 
         //if the last image in the set has loaded, add the images in order
@@ -148,10 +161,11 @@ john@toymakerlabs.com
         return loaded;
     }
 
-
+    //test comment
 	Plugin.prototype.startTransition = function(start_index) {
 	    var that = this;
 	    var slide = start_index; //current slide
+
 
 		this.interval = setInterval(function(){
             //revise to a wait function. 
@@ -170,7 +184,7 @@ john@toymakerlabs.com
             if(imagesObj["image"+slide].loaded == false){
                 that.holdup = slide;
                 slide = 0;
-               // that.wait();
+                that.wait();
             }
 
             
@@ -178,30 +192,134 @@ john@toymakerlabs.com
 	}
     var currentImage = null;
 
+    /*SIMPLE FADE TRANSITION*/
+    // Plugin.prototype.transition = function(slide_index) {
+    //     var that = this;
+    //     var t = this.options.duration;
+
+    //     console.log(slide_index);
+
+    //     if(currentImage != null){
+    //         $(currentImage).css({'z-index':'1'});
+    //         $(currentImage).stop().animate({opacity:0},400);
+    //     }
+        
+    //     var image = imagesObj["image"+slide_index].element
+    //     $(image).css({'z-index':'3'});
+    //     $(image).stop().animate({opacity:1},400);
+    //     currentImage = image;
+    // }
+
     Plugin.prototype.transition = function(slide_index) {
         var that = this;
+        var t = this.options.duration;
 
         console.log(slide_index);
 
+        //Set the image to a random corner. 
+        // $(image).css({
+        //     'left':0,
+        //     'bottom':0
+        // });
+
+
+
         if(currentImage != null){
-            $(currentImage).css({'z-index':'1'});
-            $(currentImage).animate({opacity:0},400);
+            $(currentImage).parent().css({'z-index':'1'});
+            $(currentImage).stop().animate({opacity:0},that.options.fadeSpeed);
         }
         
-        var image = imagesObj["image"+slide_index].element
-        $(image).css({'z-index':'3'});
-        $(image).animate({opacity:1},400);
+        var image = imagesObj["image"+slide_index].element;
+
+
+        // var vert = getCorner('vertical');
+        // var side = getCorner('horizontal');
+
+        var dx = that.width  - image.width;
+        var dy = that.height - image.height;
+
+        var corners = [
+            {x:0,y:0},
+            {x:dx,y:0},
+            {x:0,y:dy},
+            {x:dx,y:dy}
+        ];
+
+        var choice = Math.floor(Math.random()*4);
+        var start = corners[choice];
+
+        corners.splice(choice,1);
+        var end = corners[Math.floor(Math.random()*3)];
+
+        console.log(start.x + ","+end.x);
+
+
+        //set the div to a corner
+        //fade in the image
+        //animate the wrapper for the duration of the slide
+        $(image).animate({'opacity':1},that.options.fadeSpeed);
+
+        //set image wrapper to random corner
+        $(image).parent().css({'left':start.x,'top':start.y,'z-index':'3'});
+
+        //animate it to another random corner, except the initial corner
+        $(image).parent().animate({'left':end.x,'top':end.y,},t);
         currentImage = image;
-        
-
-        
-
     }
 
+    /*
+    X: 0 , diff_x , -diff_x
+    Y: 0, diff_y, -diff_y
+
+    */
+    /*
+
+    start at:
+    top,left
+    0,0
+    0,diff_x
+    diff_y,0
+    diff_y,diff_x
+
+    go to:
+    any choice but the last picked
+    */
+
+    /* Returns a random css property to start the corner tween */
+    function getCorner(num) {
+        var corner = {};
+        switch(num) {
+            case 0:
+                //top left
+                corner = {'top':0,'left':0}
+                break;
+            case 1:
+                //top right
+                corner = {'top':0,'left':'auto','right':0}
+                break;
+            break;
+            case 2:
+                //bottom left
+                corner = {'top':'auto','bottom':0,'left':0 }
+                break;
+            break;
+            case 3:
+                //bottom right
+                corner = {'top':'auto','bottom':0,'left':'auto','right':0}
+                break;
+            break;
+            default:
+                corner = {'top':0,'left':0}
+                break;
+            break;
+        }
+        return corner;
+        
+    }
 
     Plugin.prototype.wait = function() {
         clearInterval(this.interval);
-        console.log("paused")
+        console.log("paused");
     }
 
     Plugin.prototype.insertAt = function (index, element) {
@@ -209,7 +327,7 @@ john@toymakerlabs.com
         if (index < 0) {
             index = Math.max(0, lastIndex + 1 + index);
         }
-        $(this.element).append(element);
+        var imgWrapper = $(this.element).append(element);
         if (index < lastIndex) {
             $(this.element).children().eq(index).before($(this.element).children().last());
         }
