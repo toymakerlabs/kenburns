4ecff42060be076eb86293435e9c6cf180efe078
John <toymakerlabs@gmail.com>
2012-11-28 15:08:25 -0800

    Added Fix for Parallel Image Loading
    Enabled the parallel loading simulation using buttons. Added fix to stop animation from fading the current image. Added placeholders in code for loading display. The wait function adds a class to the image called "stalled" when all the images have loaded, the resume function finds the "stalled" image containers, and resets them, Which fixes z-index and opacity bugs.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ kenburns
 ========
 
 Jquery gallery plugin featuring animated Ken Burns style transitions. 
+
+
+Features:
+Feature detection for -webkit-transform and -moz-transform to enable smooth animation
+Alternate using standard jquery animation
--- js/kenburns.js
+++ js/kenburns.js
@@ -1,31 +1,11 @@
 /*
 KenBurns Image Rotator
-by Toymakerlabs
-john@toymakerlabs.com
-*/
-
-
-/*!
- * jQuery lightweight plugin boilerplate
  * Original author: @ajpiano
  * Further changes, comments: @addyosmani
  * Licensed under the MIT license
- */
+*/
 
 ;(function ( $, window, document, undefined ) {
-    
-    // undefined is used here as the undefined global 
-    // variable in ECMAScript 3 and is mutable (i.e. it can 
-    // be changed by someone else). undefined isn't really 
-    // being passed in so we can ensure that its value is 
-    // truly undefined. In ES5, undefined can no longer be 
-    // modified.
-    
-    // window and document are passed through as local 
-    // variables rather than as globals, because this (slightly) 
-    // quickens the resolution process and can be more 
-    // efficiently minified (especially when both are 
-    // regularly referenced in your plugin).
 
     // Create the defaults once
     var pluginName = 'Kenburns',
@@ -45,7 +25,6 @@ john@toymakerlabs.com
 
     var imagesObj = {};
     var currentSlide = 0;
-    var currentImage = null;
 
     // The actual plugin constructor
     function Plugin( element, options ) {
@@ -80,7 +59,7 @@ john@toymakerlabs.com
         this.has3d = has3DTransforms();
 
         for (i in list) {
-        	this.attachImage(list[i], "image"+i , i);
+        	//this.attachImage(list[i], "image"+i , i);
         	imagesObj["image"+i] = {};
         	imagesObj["image"+i].loaded = false;
         }
@@ -110,6 +89,11 @@ john@toymakerlabs.com
         })
 
     };
+
+
+    /*  Loading and Setup
+    ------------------------------------------------------------------------------------------------- */
+
     //Load Images in parallell but keep track of the order. 
     Plugin.prototype.attachImage = function(url,alt_text,index) {
     	var that = this;
@@ -123,14 +107,12 @@ john@toymakerlabs.com
 		img.attr('src', url);
 		img.attr('alt', alt_text);
 
-
-
         wrapper.html(img);
 
         if(this.has3d) {
             img.css({'-webkit-transform-origin':'left top'});
-            img.css({'-webkit-transform':'scale('+that.options.scale+') translate3d(0,0,0)'});
             img.css({'-moz-transform-origin':'left top'});
+            img.css({'-webkit-transform':'scale('+that.options.scale+') translate3d(0,0,0)'});
             img.css({'-moz-transform':'scale('+that.options.scale+') translate3d(0,0,0)'});
         }
 
@@ -139,15 +121,15 @@ john@toymakerlabs.com
         img.load(function() {
         	imagesObj["image"+index].element = this;
         	imagesObj["image"+index].loaded  = true;
-            imagesObj["image"+index].width  = this.width;//remove
-            imagesObj["image"+index].height = this.height;//remove
             that.insertAt(index,wrapper);
             that.resume(index);
 		});
+
 	}
 
 
     Plugin.prototype.resume = function(index){
+
         //first image has loaded
         if(index == 0) {
             this.startTransition(0);
@@ -158,6 +140,8 @@ john@toymakerlabs.com
         // it will then resume the transition.
         if(index == this.holdup) {
             //console.log("resuming");
+            $('#status').html("");
+
             this.startTransition(this.holdup);
         }
 
@@ -165,6 +149,11 @@ john@toymakerlabs.com
         //fire the complete event
         if(this.checkLoadProgress() == true){
             this.options.onLoadingComplete();
+            //reset the opacities and z indexes except the last and first
+            $(this.element).find('stalled').each(function(index){
+                $(this).css({'opacity':1,'z-index':1});
+            })
+            
         }
     }
 
@@ -178,6 +167,23 @@ john@toymakerlabs.com
         }
         return loaded;
     }
+
+
+    Plugin.prototype.wait = function() {
+        clearInterval(this.interval);
+        $('#status').html("loading");
+        //pause animation in the middle.
+        //imagesObj["image"+currentSlide].stop(); 
+         var image = imagesObj["image"+(currentSlide-1)].element;
+         $(image).parent().stop(true,true);
+         $(image).parent().addClass('stalled');
+    }
+
+
+
+
+    /*  Transitions and Movement
+    ------------------------------------------------------------------------------------------------- */
 
     //test comment
 	Plugin.prototype.startTransition = function(start_index) {
@@ -195,8 +201,10 @@ john@toymakerlabs.com
  
             if(imagesObj["image"+currentSlide].loaded == false){
                 that.holdup = currentSlide;
-                currentSlide = 0;
+                
                 that.wait();
+
+                //currentSlide = 0;
             }else {
                 that.doTransition();
             }
@@ -207,12 +215,18 @@ john@toymakerlabs.com
 	}
 
 
+    /** 
+    * chooseCorner
+    * This function chooses a random start corner and a random end corner
+    * that is different from the start. This gives a random direction effect
+    * it returns coordinates used by the transition functions. 
+    */
+   
     Plugin.prototype.chooseCorner = function() {
         var scale = this.options.scale; 
-        //determine scale and difference in width
         var image = imagesObj["image"+currentSlide].element;
-        var sw = $(image).width();//imagesObj["image"+currentSlide].width;
-        var sh = $(image).height();//imagesObj["image"+currentSlide].height;    
+        var sw = $(image).width();
+        var sh = $(image).height();
         var dx = Math.round((this.width  - (sw*scale))*100)/100;
         var dy = Math.round((this.height - (sh*scale))*100)/100;
 
@@ -223,14 +237,15 @@ john@toymakerlabs.com
             {x:1,y:1}
         ];
 
-        //Pick the first corner. Remove it from the array. Pick the second corner from the subset. 
+        //Pick the first corner. Remove it from the array 
         var choice = Math.floor(Math.random()*4);
         var start = corners[choice];
+
+        //Pick the second corner from the subset
         corners.splice(choice,1);
         var end = corners[Math.floor(Math.random()*3)];
 
-        //build the coordinates from the chosen coordinates
-
+        //build the new coordinates from the chosen coordinates
         var coordinates = {
             startX: start.x * dx,
             startY: start.y * dy,
@@ -238,87 +253,108 @@ john@toymakerlabs.com
             endY: end.y * dy * (1+(1-scale))
         }
 
-        return coordinates;//corners[Math.floor(Math.random()*3)];
-
-
+        return coordinates;
     }
 
+
+
+    /** 
+    *  Transiton3D
+    *  Transition3d Function works by setting the webkit and moz translate3d properties. These
+    *  are hardware accellerated and give a very smooth animation. Since only one animation
+    *  can be applied at a time, I wrapped the images in a div. The shorter fade is applied to
+    *  the parent, while the translation and scaling is applied to the image.
+    */
+
     Plugin.prototype.transition3d = function () {
-        var that = this;
+        var that  = this;
         var scale = this.options.scale; 
         var image = imagesObj["image"+currentSlide].element;
         var position = this.chooseCorner();
 
-        // if(currentImage != null){
-        //     $(currentImage).parent().css({'z-index':'1'});
-        //     $(currentImage).parent().animate({'opacity':0},that.options.fadeSpeed);
-        // }
 
+        //First clear any existing transition
         $(image).css({'-webkit-transition':'none'});
-        $(image).css({'-webkit-transform':'scale('+scale+') translate3d('+position.startX+'px,'+position.startY+'px,0)'});
         $(image).css({'-moz-transition':'none'});
+        $(image).css({'-webkit-transform':'scale('+scale+') translate3d('+position.startX+'px,'+position.startY+'px,0)'});
         $(image).css({'-moz-transform':'scale('+scale+') translate3d('+position.startX+'px,'+position.startY+'px,0)'});
 
+        //Set the wrapper to fully transparent and start it's animation
         $(image).parent().css({'opacity':0,'z-index':'3'});
         $(image).parent().animate({'opacity':1},that.options.fadeSpeed);
 
+        //Add the transition back in
         $(image).css({'-webkit-transition':'-webkit-transform '+(that.options.duration+that.options.fadeSpeed)+'ms '+that.options.ease3d});
-        $(image).css({'-webkit-transform':'scale(1) translate3d('+position.endX+'px,'+position.endY+'px,0)'});
         $(image).css({'-moz-transition':'-moz-transform '+(that.options.duration+that.options.fadeSpeed)+'ms '+that.options.ease3d});
+
+        //set the end position and scale, which fires the transition
+        $(image).css({'-webkit-transform':'scale(1) translate3d('+position.endX+'px,'+position.endY+'px,0)'});
         $(image).css({'-moz-transform':'scale(1) translate3d('+position.endX+'px,'+position.endY+'px,0)'});
 
-        $(image).parent().delay(that.options.duration).animate({'opacity':0},that.options.fadeSpeed, function(){
-            $(this).parent().css({'z-index':'1'});
-        });
-
-        currentImage = image;
-        that.options.onSlideComplete();
-
+        this.transitionOut();
+        this.options.onSlideComplete();
     }
 
+
+
+    /**
+     *  Transition
+     *  The regular JQuery animation function. Sets the currentSlide initial scale and position to 
+     *  the value from chooseCorner before triggering the animation. It starts the image moving to
+     *  the new position, starts the fade on the wrapper, and delays the fade out animation. Adding
+     *  fadeSpeed to duration gave me a nice crossfade so the image continues to move as it fades out
+     *  rather than just stopping.  
+     */
+
     Plugin.prototype.transition = function() {
-        var that = this;
+        var that  = this;
         var scale = this.options.scale; 
         var image = imagesObj["image"+currentSlide].element;
-        var sw = $(image).width();//imagesObj["image"+currentSlide].width;
-        var sh = $(image).height();//imagesObj["image"+currentSlide].height; 
+        var sw = $(image).width();
+        var sh = $(image).height();
         var position = this.chooseCorner();
 
-        // if(currentImage != null){
-        //      $(currentImage).parent().css({'z-index':'1'});
-        //      $(currentImage).parent().animate({'opacity':0},that.options.fadeSpeed);
-        //  }
-        //  $(image).css({'z-index':3});
-
-
-
         $(image).css({'left':position.startX,'top':position.startY,'width':sw*(scale),'height':sh*(scale)});
-        $(image).animate({'left':position.endX,'top':position.endY,'width':sw,'height':sh},that.options.duration+that.options.fadeSpeed);
+        $(image).animate({'left':position.endX,'top':position.endY,'width':sw,'height':sh}, that.options.duration + that.options.fadeSpeed);
         
         $(image).parent().css({'opacity':0,'z-index':3});
         $(image).parent().animate({'opacity':1},that.options.fadeSpeed);
 
-        $(image).parent().delay(that.options.duration).animate({'opacity':0},that.options.fadeSpeed, function(){
-            $(this).css({'z-index':1});
-        });
-
-        // $(image).css({left:position.startX,top:position.startY});
-        // $(image).animate({left:position.endX,top:position.endY},this.options.duration);
-
-        // $(image).parent().css({'z-index':'3'});
-        // $(image).parent().animate({'opacity':1},that.options.fadeSpeed);
-
-        // $(image).parent().delay(that.options.duration).animate({'opacity':0},that.options.fadeSpeed, function(){
-        //     $(this).parent().css({'z-index':'1'});
-        // });
-
-
-        // currentImage = image;
-         that.options.onSlideComplete();
-
+        this.transitionOut();
+        this.options.onSlideComplete();
     }
 
+    Plugin.prototype.transitionOut = function() {
+        var that = this;
+        var image = imagesObj["image"+currentSlide].element;
 
+       //  if(currentSlide < this.maxSlides-1){
+       //      nextSlide = currentSlide + 1;
+       //  }else {
+       //      nextSlide = 0;
+       //  }
+
+       //  //
+       // if(imagesObj["image"+nextSlide].loaded) {
+            $(image).parent().delay(that.options.duration).animate({'opacity':0},that.options.fadeSpeed, function(){
+                $(this).css({'z-index':1});
+            });
+       //}
+    }
+
+   
+
+
+
+    /*  Utility Functions
+    ------------------------------------------------------------------------------------------------- */
+
+    /** 
+     *  has3DTransforms
+     *  Tests the browser to determine support for Webkit and Moz Transforms
+     *  Creates an element, translates the element, and tests the values. If the
+     *  values return true, the browser supports 3D transformations. 
+     */
     function has3DTransforms() {
         var el = document.createElement('p'), 
             has3d,
@@ -327,7 +363,6 @@ john@toymakerlabs.com
                 'MozTransform':'-moz-transform',
             };
 
-        // Add it to the body to get the computed style.
         document.body.insertBefore(el, null);
 
         for (var t in transforms) {
@@ -341,10 +376,13 @@ john@toymakerlabs.com
         return (has3d !== undefined && has3d.length > 0 && has3d !== "none");
     }
 
-    Plugin.prototype.wait = function() {
-        clearInterval(this.interval);
-    }
-
+    /** 
+     *  insertAt
+     *  Utility function that inserts objects at a specific index
+     *  Used to maintain the order of images as they are loaded and
+     *  added to the DOM
+     */
+    
     Plugin.prototype.insertAt = function (index, element) {
         var lastIndex = $(this.element).children().size();
         if (index < 0) {
@@ -356,8 +394,6 @@ john@toymakerlabs.com
         }
     }
 
-    // A really lightweight plugin wrapper around the constructor, 
-    // preventing against multiple instantiations
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
             if (!$.data(this, 'plugin_' + pluginName)) {
